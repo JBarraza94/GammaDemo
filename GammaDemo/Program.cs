d9281599ea239a18942df30c4d9fae143a847562
Jorge Barraza <jorge@gammapartners.com>
2016-05-04 11:00:32 -0600

    Commit in Dev Branch

--- GammaDemo/Program.cs
+++ GammaDemo/Program.cs
@@ -14,6 +14,7 @@ namespace GammaDemo
         [STAThread]
         static void Main()
         {
+            Console.BackgroundColor.ToString();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
